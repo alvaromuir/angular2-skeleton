f4eca44b6dca15b90977969626839c631738ba0c
Alvaro Muir <alvaromuir@gmail.com>
2016-09-19 23:54:14 -0400

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component }    from '@angular/core';
+
+import './rxjs-operators';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <h1> Angular Skeleton </h1>
+        `
+})
+export class AppComponent { }
